5563bd37cad298a26e489d516e701bccd846e07a
milan-kharel <milan.kharel120@gmail.com>
2024-01-19 08:32:15 +0545

    first commit

---
+++ backend/endpoint.js
@@ -0,0 +1,316 @@
+var base_url = "http://192.168.1.174:8000/api/user";
+
+const API_ENDPOINTS = {
+  register: "/register/",
+  login: "/login/",
+  profile: "/profile/",
+  changePassword: "/change-password/",
+  tokenRefresh: "/token/refresh/",
+  createClass: "/class/create/",
+  viewClassList: "/class/list/",
+  deleteClass: "/class/delete/", //need to pass id here
+  createClassRecord: "/class-record/create/",
+  viewClassRecordList: "/class-record/list/",
+  viewClassRecord: "/class-record/", //need to pass id here
+  updateClassRecord: "/class-record/update/", //need to pass id here
+  deleteClassRecord: "/class-record/delete/", //need to pass id here
+};
+const token = localStorage.getItem("token");
+
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("createClassForm")
+    .addEventListener("submit", handleFormSubmit);
+});
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const title = document.getElementById("title").value;
+  const description = document.getElementById("description").value;
+
+  createClass(title, description);
+}
+
+function createClass(title, description) {
+  const url = "http://192.168.1.174:8000/api/user/class/create/";
+  const authToken = token; // Replace with your actual token
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + authToken,
+    },
+    body: JSON.stringify({
+      title: title,
+      description: description,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // Here you can add logic to update the UI after class creation
+      console.log("Success:", data);
+      location.reload();
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  fetchClasses();
+  fetchSummary();
+});
+
+function uploadFile(event) {
+  event.preventDefault();
+  console.log("entered into upload file");
+  const url = "http://192.168.1.174:8000/api/user/class-record/create/";
+  const authToken = token; // Replace with your actual token
+
+  const title = document.getElementById("upload-popup").value;
+  const classId = document.querySelector(".class-select").value;
+  const fileInput = document.querySelector(".upload-section");
+  const uploadBtn = document.querySelector(".upload-btn");
+  const audioFile = fileInput.files[0];
+
+  const formData = new FormData();
+  formData.append("title", title);
+  formData.append("class_id", classId);
+  formData.append("audio_file", audioFile);
+
+  console.log("Uploading file with data:", title, classId, audioFile);
+  uploadBtn.innerHTML = "Uploading ..";
+  uploadBtn.disabled = "true";
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      Authorization: "Bearer " + authToken,
+    },
+    body: formData,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.message === "Class record created successfully") {
+        // Handle successful upload
+        // alert("Success");
+        uploadBtn.innerHTML = "Uploaded";
+
+        location.reload();
+      } else {
+        console.log("error");
+        // Handle errors
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+function fetchClasses() {
+  console.log("entered into fetch classes");
+  const url = base_url + API_ENDPOINTS.viewClassList;
+  const authToken = token;
+
+  fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + authToken,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      updateClassSelectOptions(data);
+      updateClassSelectOptionsRecord(data);
+      updateClassesList(data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+getUserDetail();
+
+function getUserDetail() {
+  const url = base_url + API_ENDPOINTS.profile;
+  const authToken = token;
+
+  fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + authToken,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Response of the user profile is = ", data);
+      document.getElementById("username_field").innerText = `Hi, ${data.name}`;
+      document.getElementById("email_field").innerText = data.email;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+function updateClassesList(data) {
+  console.log("Updating classes list with data:", data);
+
+  const classesContainer = document.getElementById("classesList");
+
+  // Check if the data has classes
+  if (data.length === 0) {
+    classesContainer.innerHTML = "<div>No classes available</div>";
+    return;
+  }
+
+  let htmlContent = '<div class="class-wrapper">';
+  data.forEach((classItem) => {
+    let my_date = classItem.created_at;
+    let date = new Date(my_date);
+    let formattedDate = date.toISOString().split("T")[0];
+    console.log("The updated date is = " + formattedDate);
+
+    htmlContent += `<div class="class">
+                          <div class="class-title">${classItem.title}</div>
+                          <div class="class-description">${classItem.description}</div>
+                          <div class="class-date">Created at: ${formattedDate}</div>
+                      </div>`;
+  });
+
+  htmlContent += "</div>";
+  classesContainer.innerHTML = htmlContent;
+}
+
+function fetchSummary() {
+  console.log("entered into fetch summary");
+  const url = "http://192.168.1.174:8000/api/user/class-record/list/";
+  const authToken = token; // Replace with your actual token
+
+  fetch(url, {
+    method: "GET", // Changed to GET request
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + authToken,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      updateSummaryList(data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+function updateClassSelectOptions(data) {
+  console.log(data);
+  console.log("Updating class select options with data:", data);
+  const selectElement = document.querySelector(".class-select");
+
+  const selectElementRecord = document.querySelector(".class-select-record");
+
+  selectElement.innerHTML = '<option value="">Select class</option>'; // Clear existing options
+
+  data.forEach((classItem) => {
+    const optionElement = document.createElement("option");
+    optionElement.value = classItem.id; // Replace 'classId' with the correct property name
+    optionElement.textContent = classItem.title;
+    selectElement.appendChild(optionElement);
+  });
+}
+
+function updateClassSelectOptionsRecord(data) {
+  console.log(data);
+  console.log("Updating class select options with data:", data);
+  const selectElement = document.querySelector(".class-select-record");
+
+  selectElement.innerHTML = '<option value="">Select class</option>'; // Clear existing options
+
+  data.forEach((classItem) => {
+    const optionElement = document.createElement("option");
+    optionElement.value = classItem.id; // Replace 'classId' with the correct property name
+    optionElement.textContent = classItem.title;
+    selectElement.appendChild(optionElement);
+  });
+}
+
+function getSummaryName(id) {
+  const url = base_url + API_ENDPOINTS.viewClassList;
+  const authToken = token;
+
+  // Return a new Promise
+  return new Promise((resolve, reject) => {
+    fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + authToken,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Find the class with the given id
+        const classItem = data.find((item) => item.id === id);
+
+        if (classItem) {
+          // If the class is found, resolve the Promise with its title
+          resolve(classItem.title);
+        } else {
+          reject("Class not found");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        reject(error);
+      });
+  });
+}
+
+function updateSummaryList(data) {
+  console.log("This is the summary list :", data);
+
+  const classesContainer = document.getElementById("recordingList");
+
+  // Check if the data has classes
+  if (data.length === 0) {
+    classesContainer.innerHTML = "<div>No recording summary available</div>";
+    return;
+  }
+
+  let htmlContent = '<div class="recordings">';
+  data.forEach((classItem) => {
+    let recordClassName;
+    let my_date = classItem.created_at;
+    let date = new Date(my_date);
+    let formattedDate = date.toISOString().split("T")[0];
+    console.log("The updated date is = " + formattedDate);
+
+    getSummaryName(classItem.class_id)
+      .then((className) => {
+        recordClassName = className;
+        console.log("The classname is = " + className);
+      })
+      .catch((error) => {
+        console.log("Error getting class name:", error);
+      });
+
+    htmlContent += `
+    <div class="record">
+    <div class="record-visible">
+      <div class="record-title">${classItem.title}</div>
+    </div>
+    <div class="record-info">
+    ${classItem.summary} 
+    </div>
+  </div>`;
+  });
+
+  // htmlContent += "</div>";
+  classesContainer.innerHTML = htmlContent;
+}
---
+++ dashboard.js
@@ -0,0 +1,103 @@
+//dashboard animaion
+const records = document.querySelectorAll(".record");
+setTimeout(() => {
+  records.forEach((record) => {
+    record.classList.add("show-section");
+  });
+}, 300);
+
+records.forEach((record, index) => {
+  record.style.transitionDelay = 0.1 * index + "s";
+});
+
+//for Record modal
+const openModal = document.querySelector(".create-btn");
+const closeModal = document.querySelector(".close-modal");
+const recordModal = document.querySelector(".record-modal");
+
+openModal.addEventListener("click", () => {
+  recordModal.showModal();
+});
+closeModal.addEventListener("click", () => {
+  recordModal.close();
+});
+
+//For Upload Modal
+const openUploadModal = document.querySelector(".add-btn");
+const closeUploadModal = document.querySelector(".upload-cancel");
+const uploadModal = document.querySelector(".upload-modal");
+
+openUploadModal.addEventListener("click", () => {
+  uploadModal.showModal();
+});
+closeUploadModal.addEventListener("click", () => {
+  uploadModal.close();
+});
+
+//Recorder
+const recordBtn = document.querySelector(".record-btn");
+recordBtn.addEventListener("click", () => {
+  if (recordBtn.innerText == "Start") {
+    recordBtn.innerText = "Stop";
+  } else {
+    recordBtn.innerText = "Start";
+  }
+});
+
+// Delete Modal
+const deletebtns = document.querySelectorAll(".delete-btn");
+const cancelDeletebtns = document.querySelectorAll(".cancel-delete");
+const deleteModal = document.querySelector(".delete-modal");
+
+deletebtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    deleteModal.showModal();
+  });
+});
+cancelDeletebtns.forEach((cancelbtn) => {
+  cancelbtn.addEventListener("click", () => {
+    deleteModal.close();
+  });
+});
+
+// Record Info Dropdown
+const recordInfo = document.querySelectorAll(".record-info");
+const dropdownBtn = document.querySelectorAll(".dropdown-btn");
+const recordTitle = document.querySelectorAll(".record-title");
+
+dropdownBtn.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    btn.classList.toggle("rotate");
+    recordInfo[index].classList.toggle("show-record-info");
+    recordTitle[index].classList.toggle("theme-color");
+    recordTitle[index].classList.toggle("bigger-font");
+  });
+});
+
+//Profile Modal
+const profileBtn = document.querySelector(".profile-section");
+const closeProfileModal = document.querySelector(".close-profile-modal");
+const profileModal = document.querySelector(".profile-modal");
+
+profileBtn.addEventListener("click", () => {
+  profileModal.showModal();
+});
+closeProfileModal.addEventListener("click", () => {
+  profileModal.close();
+});
+
+//Intersection observer
+const cardsObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("reveal-cards");
+    } else {
+      entry.target.classList.remove("reveal-cards");
+    }
+  });
+});
+
+const hiddenCards = document.querySelectorAll(".class");
+hiddenCards.forEach((elem) => {
+  cardsObserver.observe(elem);
+});
